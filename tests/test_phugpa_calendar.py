342d3c65ea1968c8fed86bc7666368c89ef7c841
Simon Weiss <wonder@simonweiss.space>
2024-06-16 18:27:30 +0400

    Failing test for Henning animal-element 1800-2800

--- tests/test_phugpa_calendar.py
+++ tests/test_phugpa_calendar.py
@@ -1,14 +1,15 @@
+import re
 import unittest
 from datetime import datetime
 
-from namkha_calculator.astrology import Animal, Element
 from namkha_calculator import phugpa_calendar as pc
+from namkha_calculator.astrology import Animal, Element
 
 
 class TestPhugpaCalendar(unittest.TestCase):
     def test_year_characteristics(self):
         test_year = pc.Year(
-            tibetan_year_number=127+2024,
+            tibetan_year_number=127 + 2024,
             animal=Animal.DRAGON,
             element=Element.WOOD,
             mewa_number=3,
@@ -16,3 +17,26 @@ class TestPhugpaCalendar(unittest.TestCase):
         test_date = datetime(year=2024, month=6, day=1)
 
         self.assertEqual(test_year, pc.year_characteristics(test_date))
+
+    def test_year_element_animal_against_henning(self):
+        RE_HENNING_YEAR = r"New Year: \d*, ([A-Z][a-z]*)-[a-z]*-([A-Z][a-z]*)"
+        ELEMENT_NAMES_MAP = {
+            "Iron": "Metal",
+            "Water": "Water",
+            "Wood": "Wood",
+            "Fire": "Fire",
+            "Earth": "Earth",
+        }
+        for test_western_year in range(1800, 2801):
+            with self.subTest(western_year=test_western_year):
+                test_date = datetime(year=test_western_year, month=6, day=1)
+                test_year_characterisitcs = pc.year_characteristics(test_date)
+                with open(f"tests/data/Henning/pl_{test_western_year}.txt") as file:
+                    file.readline()
+                    match = re.match(RE_HENNING_YEAR, file.readline())
+                    self.assertEqual(
+                        test_year_characterisitcs.element, Element(ELEMENT_NAMES_MAP[match.group(1)])
+                    )
+                    self.assertEqual(
+                        test_year_characterisitcs.animal, Animal(match.group(2))
+                    )
