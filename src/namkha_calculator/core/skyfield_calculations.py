c80d52ee20a32ba117ccd1410f17c6e6f5e30738
Simon Weiss <wonder@simonweiss.space>
2026-06-01 13:44:47 +0400

    Make Skyfield lazy loading thread-safe; fix typing erreor

--- src/namkha_calculator/core/skyfield_calculations.py
+++ src/namkha_calculator/core/skyfield_calculations.py
@@ -4,6 +4,7 @@ Calculations of twilights and julian dates for 'calendar' module using Skyfield
 
 import datetime as dt
 import os
+from functools import lru_cache
 from typing import TYPE_CHECKING
 
 import pytz
@@ -14,32 +15,19 @@ if TYPE_CHECKING:
     from .astrology import Location
 
 
-_sf_loader = None
-_sf_timescale = None
-_sf_ephemeris = None
-
-
+@lru_cache(maxsize=None)
 def _get_loader():
-    global _sf_loader
-    if _sf_loader is None:
-        _sf_loader = Loader(
-            str(os.path.dirname(os.path.abspath(__file__))), verbose=False
-        )
-    return _sf_loader
+    return Loader(str(os.path.dirname(os.path.abspath(__file__))), verbose=False)
 
 
+@lru_cache(maxsize=None)
 def _get_timescale():
-    global _sf_timescale
-    if _sf_timescale is None:
-        _sf_timescale = _get_loader().timescale()
-    return _sf_timescale
+    return _get_loader().timescale()
 
 
+@lru_cache(maxsize=None)
 def _get_ephemeris():
-    global _sf_ephemeris
-    if _sf_ephemeris is None:
-        _sf_ephemeris = _get_loader()("de440_filtered.bsp")
-    return _sf_ephemeris
+    return _get_loader()("de440_filtered.bsp")
 
 
 def jd_to_datetime(jd: float) -> dt.datetime:
@@ -48,7 +36,7 @@ def jd_to_datetime(jd: float) -> dt.datetime:
 
 def civil_twilight_boundaries(
     date: dt.date, pytz_tz: pytz.BaseTzInfo, location: "Location"
-) -> list[dt.datetime]:
+) -> tuple[dt.datetime, dt.datetime]:
     """
     Returns the start and end times of civil twilight
     for the given date and location in UTC.
@@ -76,4 +64,4 @@ def civil_twilight_boundaries(
             f"lat={location.latitude:.2f}, got {len(boundaries)}. "
             "Location may be within polar day/night on this date."
         )
-    return boundaries
+    return boundaries[0], boundaries[1]
